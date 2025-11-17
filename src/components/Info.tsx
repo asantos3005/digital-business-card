3a85c4ba8545bb9adb4e735f444f375051f846e9
Andrew <andrewsan321@gmail.com>
2025-11-17 16:37:19 +1000

    change profile photo to smaller more focused and reduce overall card size

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -2,7 +2,7 @@
 export default function Info(){
     return(
         <>
-        <img  className="profile-photo" src="/myphoto.jpeg" alt="my photo" />
+        <img  className="profile-photo" src="/myphotoprofile.jpg" alt="my photo" />
         <h1>Andrew Santos</h1>
         <h2>Full Stack Developer</h2>
         <h3>andrewsanprojects.com</h3>
